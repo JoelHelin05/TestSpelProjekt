30b7286e1259183ac7e02fa9b57b4be5be86f633
Helin <joel.helin@skola.gavle.se>
2025-01-27 21:17:01 +0100

    part 5

--- TestSpel/src/entity/Player.java
+++ TestSpel/src/entity/Player.java
@@ -15,10 +15,16 @@ public class Player extends Entity{
 	GamePanel gp;
 	KeyHandler keyH;
 	
+	public final int screenX;
+	public final int screenY;
+	
 	public Player(GamePanel gp, KeyHandler keyH) {
 		
 		this.gp = gp;
 		this.keyH = keyH;
+		
+		screenX = gp.screenWidth/2 - (gp.tileSize/2);
+		screenY = gp.screenHeight/2 - (gp.tileSize/2);
 		
 		setDefaultValues();
 		getPlayerImage();
@@ -26,8 +32,8 @@ public class Player extends Entity{
 	
 	public void setDefaultValues() {
 		
-		x = 100;
-		y = 100;
+		worldX = 100;
+		worldY = 100;
 		speed = 4;
 		direction = "down";
 	}
@@ -60,19 +66,19 @@ public class Player extends Entity{
 
 			if(keyH.upPressed == true) {
 				direction = "up";
-				y -= speed;
+				worldY -= speed;
 			}
 			else if (keyH.downPressed == true) {
 				direction = "down";
-				y += speed;
+				worldY += speed;
 			}
 			else if (keyH.leftPressed == true) {
 				direction = "left";
-				x -= speed;
+				worldX -= speed;
 			}
 			else if (keyH.rightPressed == true) {
 				direction = "right";
-				x += speed;
+				worldX += speed;
 			}
 			spriteCounter++;
 			//Animations hastighet
@@ -151,6 +157,6 @@ public class Player extends Entity{
 			break;
 		}
 		
-		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+		g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 	}
 }
